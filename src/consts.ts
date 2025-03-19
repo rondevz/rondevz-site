2e9dc9f71eb9363dbf747d8c169a4db6826fd2ea
Ronald Pereira <ronaldpereira01@gmail.com>
2025-03-18 21:33:01 -0400

    add: layout is looking good

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "rondevz";
+export const SITE_DESCRIPTION =
+	"Welcome to my website! I am a computer engineer and a software developer that loves code, ganming and music.";
