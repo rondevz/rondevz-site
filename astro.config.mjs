a415bfa64ba849177391d074dc1cde0f47602207
Ronald Pereira <ronaldpereira01@gmail.com>
2025-04-04 21:56:43 -0400

    feat: add UNAMI_ID environment variable support and integrate astro-umami package

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,13 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
+import umami from "@yeskunall/astro-umami";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://example.com",
-	integrations: [mdx(), sitemap(), react()],
-
-	// @ts-ignore
-	plugins: [tailwindcss()],
+	integrations: [mdx(), sitemap(), react(), umami({ id: import.meta.env.UNAMI_ID })],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
